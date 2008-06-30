e57861d85702dcf14496c79482c187572edb720f
Andy Stewart <boss@airbladesoftware.com>
2008-06-30 12:45:33 +0000

    Pads degrees and minutes to full width, not just milli-minutes.

--- lib/air_blade/geo_tools/form_helpers.rb
+++ lib/air_blade/geo_tools/form_helpers.rb
@@ -6,21 +6,21 @@ module AirBlade
         text_field("#{method}_degrees", options.merge(
           :id        => "#{@object_name}_#{method}_degrees",
           :name      => "#{@object_name}[#{method}_degrees]",
+          :value     => padded_value("#{method}_degrees", 2),
           :maxlength => 2 )) +
         '&deg;' +
 
         text_field("#{method}_minutes", options.merge(
           :id        => "#{@object_name}_#{method}_minutes",
           :name      => "#{@object_name}[#{method}_minutes]",
+          :value     => padded_value("#{method}_minutes", 2),
           :maxlength => 2 )) +
         '.' +
 
         text_field("#{method}_milli_minutes", options.merge(
           :id        => "#{@object_name}_#{method}_milli_minutes",
           :name      => "#{@object_name}[#{method}_milli_minutes]",
-          # It would be better if we could do this post-processing
-          # in the first argument to the text_field method.
-          :value     => (@object ? (@object.send("#{method}_milli_minutes").to_s.rjust 3, '0') : nil),
+          :value     => padded_value("#{method}_milli_minutes", 3),
           :maxlength => 3 )) +
         '&prime;' +
 
@@ -34,21 +34,21 @@ module AirBlade
         text_field("#{method}_degrees", options.merge(
           :id        => "#{@object_name}_#{method}_degrees",
           :name      => "#{@object_name}[#{method}_degrees]",
+          :value     => padded_value("#{method}_degrees", 3),
           :maxlength => 3 )) +
         '&deg;' +
 
         text_field("#{method}_minutes", options.merge(
           :id        => "#{@object_name}_#{method}_minutes",
           :name      => "#{@object_name}[#{method}_minutes]",
+          :value     => padded_value("#{method}_minutes", 2),
           :maxlength => 2 )) +
         '.' +
 
         text_field("#{method}_milli_minutes", options.merge(
           :id        => "#{@object_name}_#{method}_milli_minutes",
           :name      => "#{@object_name}[#{method}_milli_minutes]",
-          # It would be better if we could do this post-processing
-          # in the first argument to the text_field method.
-          :value     => (@object ? (@object.send("#{method}_milli_minutes").to_s.rjust 3, '0') : nil),
+          :value     => padded_value("#{method}_milli_minutes", 3),
           :maxlength => 3 )) +
         '&prime;' +
 
@@ -56,6 +56,14 @@ module AirBlade
         select("#{method}_hemisphere", %w( E W ), {}, options.merge(
           :id       => "#{@object_name}_#{method}_hemisphere",
           :name     => "#{@object_name}[#{method}_hemisphere]" ))
+      end
+
+      private
+
+      # NOTE: It would be better if we could do this post-processing
+      # in the first argument to the text_field method.
+      def padded_value(method, width)
+        @object.send(method).to_s.rjust(width, '0') if @object
       end
 
     end

d3b73cf7b7e2c67ef2d27737f9b0f495312db9a0
Andy Stewart <boss@airbladesoftware.com>
2008-09-02 09:31:52 +0000

    Removed redundant code that was actually preventing object[] naming from working on forms.

--- lib/air_blade/geo_tools/form_helpers.rb
+++ lib/air_blade/geo_tools/form_helpers.rb
@@ -3,56 +3,20 @@ module AirBlade
     module FormHelpers
 
       def latitude_field(method, options = {}, html_options = {})
-        text_field("#{method}_degrees", options.merge(
-          :id        => "#{@object_name}_#{method}_degrees",
-          :name      => "#{@object_name}[#{method}_degrees]",
-          :maxlength => 2 )) +
-        '&deg;' +
-
-        text_field("#{method}_minutes", options.merge(
-          :id        => "#{@object_name}_#{method}_minutes",
-          :name      => "#{@object_name}[#{method}_minutes]",
-          :maxlength => 2 )) +
-        '.' +
-
-        text_field("#{method}_milli_minutes", options.merge(
-          :id        => "#{@object_name}_#{method}_milli_minutes",
-          :name      => "#{@object_name}[#{method}_milli_minutes]",
-          :maxlength => 3 )) +
-        '&prime;' +
-
+        text_field("#{method}_degrees",       options.merge(:maxlength => 2)) + '&deg;'   +
+        text_field("#{method}_minutes",       options.merge(:maxlength => 2)) + '.'       +
+        text_field("#{method}_milli_minutes", options.merge(:maxlength => 3)) + '&prime;' +
         # Hmm, we pass the options in the html_options position.
-        select("#{method}_hemisphere", %w( N S ), {}, options.merge(
-          :id       => "#{@object_name}_#{method}_hemisphere",
-          :name     => "#{@object_name}[#{method}_hemisphere]" ))
+        select("#{method}_hemisphere", %w( N S ), {}, options)
       end
 
       def longitude_field(method, options = {}, html_options = {})
-        text_field("#{method}_degrees", options.merge(
-          :id        => "#{@object_name}_#{method}_degrees",
-          :name      => "#{@object_name}[#{method}_degrees]",
-          :maxlength => 3 )) +
-        '&deg;' +
-
-        text_field("#{method}_minutes", options.merge(
-          :id        => "#{@object_name}_#{method}_minutes",
-          :name      => "#{@object_name}[#{method}_minutes]",
-          :maxlength => 2 )) +
-        '.' +
-
-        text_field("#{method}_milli_minutes", options.merge(
-          :id        => "#{@object_name}_#{method}_milli_minutes",
-          :name      => "#{@object_name}[#{method}_milli_minutes]",
-          :maxlength => 3 )) +
-        '&prime;' +
-
+        text_field("#{method}_degrees",       options.merge(:maxlength => 3)) + '&deg;'   +
+        text_field("#{method}_minutes",       options.merge(:maxlength => 2)) + '.'       +
+        text_field("#{method}_milli_minutes", options.merge(:maxlength => 3)) + '&prime;' +
         # Hmm, we pass the options in the html_options position.
-        select("#{method}_hemisphere", %w( E W ), {}, options.merge(
-          :id       => "#{@object_name}_#{method}_hemisphere",
-          :name     => "#{@object_name}[#{method}_hemisphere]" ))
+        select("#{method}_hemisphere", %w( E W ), {}, options)
       end
-
-      private
 
     end
   end
